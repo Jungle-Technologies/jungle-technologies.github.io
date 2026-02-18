f060d4d95bea55c4ab10d786f35acb798ca8b206
Jungle-Technologies <>
2026-02-18 13:20:39 +0000

    Update from https://github.com/Jungle-Technologies/jungle-tech/commit/90fca5d3f3ce3a2de7797378fb0624b3e41eac8b

---
+++ _astro/HeroSection.astro_astro_type_script_index_0_lang.C3lCAyAm.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.CFwdEl9y.js";document.addEventListener("astro:page-load",()=>{e(document.getElementById("heroImage"),{scale:[.4,1],opacity:[0,1]},{ease:"circInOut",duration:.6})});
