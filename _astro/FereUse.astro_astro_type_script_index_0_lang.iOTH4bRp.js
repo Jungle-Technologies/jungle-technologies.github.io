dfbf93e6c20dae145e836c07674e3df8f58af41a
Jungle-Technologies <>
2026-06-04 05:04:41 +0000

    Update from https://github.com/Jungle-Technologies/jungle-tech/commit/9c8e838d33fa9a78d5dca4fdec0e452b71460baf

---
+++ _astro/FereUse.astro_astro_type_script_index_0_lang.iOTH4bRp.js
@@ -0,0 +1,1 @@
+import{i}from"./case-study-scroll.Df2scl02.js";import"./index.2sZdC5_f.js";import"./is-svg-element.DGwM38eu.js";i();
--- _astro/HeroSection.astro_astro_type_script_index_0_lang.C3lCAyAm.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index.CFwdEl9y.js";document.addEventListener("astro:page-load",()=>{e(document.getElementById("heroImage"),{scale:[.4,1],opacity:[0,1]},{ease:"circInOut",duration:.6})});
